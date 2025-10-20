26239f403dfcbec821763d333c0b49de3fa21989
DucViet0301 <vietdao464@gmail.com>
2025-10-20 22:22:41 +0700

    Update HomeController.cs

---
+++ WebBanLapTop/Areas/Admin/Controllers/ProductDiscountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanLapTop.Areas.Admin.Controllers
+{
+    public class ProductDiscountController : Controller
+    {
+        // GET: Admin/ProductDiscount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanLapTop/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebBanLapTop.Models;
+
+namespace WebBanLapTop.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        DatabaseDataContext db = new DatabaseDataContext();
+        public ActionResult Index(int ?page)
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var items = db.tb_products.OrderByDescending(x => x.product_id)
+                                      .Skip((pageNumber-1)*pageSize)
+                                      .Take(pageSize)
+                                      .ToList();
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = pageNumber;
+
+            return View(items);
+        }
+    }
+}
